67f26b31756c45383d9ac392a436fc3bf8d8f612
Aydın Başkaya <aydin.baskaya@metu.edu.tr>
2017-04-13 15:28:05 +0300

    code block sequence edit for function

--- thesis/matlab-optimization/total_mass.m
+++ thesis/matlab-optimization/total_mass.m
@@ -38,6 +38,183 @@ function result = total_mass(r_mean,g,rpm,J,t_o,t_i,lc,groove,width_ratio,Nt,Np,
 %-------------------------------------------------------------------------------------------------------
 
 
+%% CONSTANTS 
+
+% Natural air Cooling for J=5.5 A/mm^2
+% Forced air cooling for J=7 A/mm^2
+% Forced water cooling for J=9 A/mm^2
+
+% thermal constants(from optimization)
+
+alpha_Cu=3.9-E03 ;
+rho_cu=1.7-E08;
+a_bar=40; %heat transfer coefficient(?) in W/m^2.K
+a_stat=25; %heat transfer coefficient(?) in W/m^2.K
+a_conc=60; %heat transfer coefficient(?) in W/m^2.K
+lambda_steel=54; %lambda_steel: thermal conductivity of steel in W/m.K
+lambda_alum=250; % lambda_alum: thermal conductivity of aluminium in W/m.K
+lambda_cu_coil=400; % lambda_cu_coil: thermal conductivity of copper along coil in W/m.K
+lambda_cu_ver=1.8; % lambda_cu_ver: thermal conductivity of copper in vertical in W/m.K
+lambda_epoxy=1.3; % lambda_epoxy:thermal conductivity of epoxy in W/m.K 
+lambda_pm=9; % lambda_pm: lambda_pm:thermal conductivity of PM in W/m.K
+k=0.262; % k: thermal conductiviy of air in W/m.K
+alpha_ep2air=40 ; % alpha_ep2air: epoxy to air heat transfer coefficient in W/m^2.K
+alpha_st2air=40 ; % alpha_st2air: steel to air heat transfer coefficient in W/m^2.K
+alpha_alum2air=40 ;% alpha_alum2air: aluminium to air heat transfer coefficient in W/m^2.K
+h_epoxy=0.0005 ; % h_epoxy: height of the epoxy layer
+h_band=0.01 ; % h_band: height of steel band(jubilee clip)
+w_band=0.04; % w_band: axial width of the steel band
+t_disc=?? ... continue 
+    
+mu_0=1.257E-06 ; % constant
+
+%--------------------------------------------------------------------------------------------------------------------------
+
+
+%% [I_ph_rms] Current per phase(rms) calculation
+
+%% ----------Definition of the parameters/variables----------
+
+%I_coil : current in one coil
+% J:curent density, a_cond=cross section area of the conductor 
+% a_window: effective area of winding window
+%h_w : height of the winding,  kf: fill factor(1 for concentrated windings)
+
+%% Calculation part
+
+tau_p= r_mean*2*pi/Np ; 
+tau_c=tau_p*(4/3);
+width_winding=pitch_ratio*tau_c;
+a_window=h_w*width_winding*kf; 
+a_cond=a_window/Nt;  
+I_coil=J*a_cond*1000000; 
+
+I_ph_rms= I_coil*n_branch;  %%Resulting equation
+
+%------------------------------------------------------------------------------------------------------------------------------
+
+
+%--------------------------------------------------------------------------------------------------------------------------
+%% Flux Density Parameters calculation (Airgap, Spacer and steel flux densities)
+
+%% ----------Definition of the parameters/variables----------
+
+% B_ag: flux density in air-gap , B_ag_l: flux density in air-gap included leakage flux
+% B_ag_nl: flux density in air-gap not included leakage flux
+% leakage_insert : adjustment for enable leakage flux effect i.e leakage_insert=1---> leakage flux enabled in calculation otherwise not enabled
+% NI: mmf
+% Br: magnet remanent flux density (constant)
+% phi_ag_nl: airgap flux not included leakage flux
+% phi_ag_l: airgap flux included leakage(first row of flux_l matrix) , magnet_width : magnet width 
+% R: reluctance matrix
+% inverse_R: inverse of reluctance matrix[2x2] , tot_mmf_matrix: total mmf matrix[2x1] , flux_l: flux matrix included leakage effect 
+% S_st: steel reluctance 
+% S_st_A: steel reluctance part A , S_st_C: steel reluctance part C  
+% l_cl:magnet to steel web clearence , tau_pw: web pole pitch
+% l_ws_web: winding to steel web clearence (user defined variable)
+% r_w: web radius
+% S_sp: spacer reluctance
+% inter_area: intermodule area , mu_st: steel relative permeability (constant)
+% S_PM_o: top PM reluctance , S_ag: airgap reluctance ,S_I: flux leakage included reluctance 
+% SI_1: reluctance matrix part one, SI_2: reluctance matrix part two
+% mu_r: magnet relative permeability (constant)
+% B_sp: flux density in spacer(intermodule) , B_sp_l: flux density in spacer included leakage flux
+% B_sp_nl: flux density in spacer(intermodule) not included leakage flux
+% phi_sp_nl: spacer flux not included leakage
+% phi_sp_l: spacer flux included leakage (second row of flux_l matrix )
+% A_sp_o: intermodule area
+% B_st: flux density in steel(intramodule) , B_st_l: flux density in steel included leakage flux
+% B_st_nl: flux density in steel(intramodule) not included leakage flux
+% phi_st_l : steel flux included leakage , A_st: intramodule(web) area 
+% phi_st_nl: steel flux not included leakage 
+
+%% Calculation part
+
+%----for air-gap region-------%
+
+r_i= r_mean- l_magnet/2 ;
+r_o= r_mean+ l_magnet/2 ; 
+NI=Br*h_m/(mu_0*mu_r) ; 
+magnet_width= width_ratio*tau_p;
+SI_2= pi/(2*l_magnet*mu_0) ; 
+SI_1=(tau_p-magnet_width)/(mu_0*l_magnet*(0.5*h_w+g+h_m-groove)) ;  
+S_I=SI_1+SI_2 ;  
+S_ag=(h_w+2*g)/(l_magnet*magnet_width*mu_0) ; 
+S_PM_o= h_m/(l_magnet*magnet_width*mu_0*mu_r)+0.5*t_o/(l_magnet*magnet_width*mu_0*mu_r) ; 
+
+R(1,1)=2*S_PM_o*(1+2*S_ag/S_I)+S_ag ;  
+
+inter_area=t_o*l_magnet ;
+S_sp=(tau_p/(inter_area*mu_0*mu_st))+(groove_c/(inter_area*mu_0)) ; 
+
+R(1,2)= S_sp ;  
+
+l_cl= width_winding+l_ws_web ; 
+r_w=r_i-l_cl-lc ; 
+diameter_web=r_w;
+tau_pw= 2*pi*r_w/Np ;  
+S_st_A=(l_magnet+2*l_cl+lc)/(t_o*(tau_p+tau_pw)*mu_0*mu_st) ;  
+S_st_C=(2*(h_m+g)+h_w+t_o)/(lc*tau_pw*mu_0*mu_st) ; 
+S_st=2*S_st_A+S_st_C ; 
+R(2,1) = R(1,1)+(1+2*S_ag/S_I)*S_st ; 
+R(2,2)=-2*S_st ; 
+
+inverse_R = inv(R);  
+flux_l= inverse_R*tot_mmf_matrix ; 
+
+phi_ag_l= flux_l(1,:) ; 
+B_ag_l= phi_ag_l/l_magnet/magnet_width ; 
+
+phi_ag_nl=(S_sp+2*S_st)*NI/(S_st*(2*S_PM_o+S_ag+0.5*S_sp)+0.5*(S_sp*(2*S_PM_o+S_ag))) ; 
+B_ag_nl= phi_ag_nl/magnet_width/l_magnet; 
+
+if (leakage_insert==1)  
+    B_ag=B_ag_l ;  
+else
+    B_ag=B_ag_nl; 
+end
+
+%------end of airgap region flux density calculation----------------%
+
+%-----for spacer region---------%
+
+A_sp_o=t_i*l_magnet ; 
+
+phi_sp_nl= S_sp*NI/(S_st*(2*S_PM_o+S_ag+0.5*S_sp)+0.5*(S_sp*(2*S_PM_o+S_ag))) ;
+B_sp_nl= phi_sp_nl/A_sp_o ; 
+
+phi_sp_l=flux_l(2,:) ; % 
+B_sp_l=phi_sp_l/A_sp_o ; 
+
+if (leakage_insert==1)  
+    B_sp =B_sp_l ;  
+else
+    B_sp =B_sp_nl ; 
+end
+
+%------ end of spacer region flux density calculation---------%
+
+%-----for steel region---------%
+
+A_st=tau_pw*lc ; 
+
+phi_st_nl=S_st*NI/(S_st*(2*S_PM_o+S_ag+0.5*S_sp)+0.5*(S_sp*(2*S_PM_o+S_ag))) ; 
+B_st_nl=phi_st_nl/A_st ; 
+
+
+phi_st_l=phi_ag_l*(1+2*S_ag/S_I)-2*phi_sp_l ; 
+B_st_l=phi_st_l/A_st ;  
+
+if (leakage_insert==1)  
+    B_st =B_st_l ;  
+else
+    B_st =B_st_nl ; 
+end
+
+%------ end of steel region flux density calculation---------%
+
+
+%----------------------------------------------------------------------------------------------------
 %% [V_ph_rms] Voltage per phase(rms) calculation
 
 %% ----------Definition of the parameters/variables----------
@@ -68,9 +245,17 @@ function result = total_mass(r_mean,g,rpm,J,t_o,t_i,lc,groove,width_ratio,Nt,Np,
 
 %% Calculation part
 
+Nc=(3/4)*Np ;
+coil_phase=Nc/m; 
+N_series=coil_phase/n_branch; 
+width_winding=pitch_ratio*tau_c; 
+theta_dif=width_winding/r_mean;  
+theta_o= tau_c/r_mean/2;   
+theta_i= theta_o-theta_dif; 
+
 l_mm=h_w+2*g ; 
 l_ss=l_mm+2*(h_m-groove) ;  
-mu_0=1.257E-06 ; % constant
+
 B_a=mu_0*Nt/l_ss ; 
 flux_lnk=(2*B_a*Nt*((0.5*(r_o^2-r_i^2)*tand(theta_o))-(width_winding*l_magnet)))+(2*B_a*Nt*width_winding*l_magnet/3) ;  
 k_ind=1; % constant
@@ -78,7 +263,6 @@ L_coil= k_ind*flux_lnk ;
 f=rpm/60*Np/2 ; 
 w_e=2*pi*f ; 
 X_ph=w_e*L_coil*(N_series/n_branch); 
-Nc=(3/4)*Np ; 
 l_coil_structure=(r_mean+0.5*(l_magnet+width_winding))*2*pi/Nc;
 l_coil_middle=l_magnet+width_winding; 
 l_coil_end=(r_mean-0.5*(l_magnet+width_winding))*2*pi/Nc; 
@@ -86,16 +270,6 @@ l_t=l_coil_end+2*l_coil_middle+l_coil_structure-2*width_winding;
 R_coil=rho_cu*l_t*Nt/a_cond; 
 R_amb=R_coil*N_series/n_branch  ;
 R_ph_th=R_amb*(1+alpha_Cu*dT); 
-coil_phase=Nc/m; 
-N_series=coil_phase/n_branch; 
-width_winding=pitch_ratio*tau_c; 
-theta_dif=width_winding/r_mean;  
-tau_p= r_mean*2*pi/Np ; 
-tau_c=tau_p*(4/3);
-theta_o= tau_c/r_mean/2;   
-theta_i= theta_o-theta_dif; 
-r_i= r_mean- l_magnet/2 ;
-r_o= r_mean+ l_magnet/2 ;  
 magnet_h1=1.27324*B_ag*sind(width_ratio*pi/2); 
 flux_lnk_peak=k_leak*magnet_h1*(r_o^2-r_i^2)*(cosd(Np*theta_i/2)-cosd(Np*theta_o/2))/(theta_dif*((Np/2)^2)) ;  
 w_m=(rpm*2*pi)/60 ; 
@@ -109,26 +283,7 @@ V_ph_rms = E_ph_rms*cosd(lambda)-I_ph_rms(R_ph_th*cosd(phi)+X_ph*sind(phi)) ;  %
 %--------------------------------------------------------------------------------------------------------------------------
 
 
-%--------------------------------------------------------------------------------------------------------------------------
-%% [I_ph_rms] Current per phase(rms) calculation
 
-%% ----------Definition of the parameters/variables----------
-
-%I_coil : current in one coil
-% J:curent density, a_cond=cross section area of the conductor 
-% a_window: effective area of winding window
-%h_w : height of the winding,  kf: fill factor(1 for concentrated windings)
-
-%% Calculation part
-
-width_winding=pitch_ratio*tau_c;
-a_window=h_w*width_winding*kf; 
-a_cond=a_window/Nt;  
-I_coil=J*a_cond*1000000; 
-
-I_ph_rms= I_coil*n_branch;  %%Resulting equation
-
-%------------------------------------------------------------------------------------------------------------------------------
 
 
 %--------------------------------------------------------------------------------------------------------------------------
@@ -165,6 +320,8 @@ P_o= m*V_ph_rms*I_ph_rms; %%Resulting equation
 
 %% Calculation part
 
+turn_strand=Nt/strand ; 
+h_coil_i=(h_w*1000-2*t_epoxy)/turn_strand ;
 eddy_magnet= 57.65*l_magnet*magnet_width*Np*2 ; % eddy_magnet: magnet surface eddy current loss
 leakage_loss=0  ;% constant
 coil_area_i=(width_winding*1000-2*t_epoxy)*(h_w*1000-2*t_epoxy)/Nt ; 
@@ -177,8 +334,6 @@ else
     t_insulation=0;
 end
 
-turn_strand=Nt/strand ; 
-h_coil_i=(h_w*1000-2*t_epoxy)/turn_strand ;   
 h_copper=h_coil_i-2*t_insulation ;  
 ag_loss=2*l_magnet*Nt*(((l_magnet/2000)^3)*(B_ag_l^2)*(w_e^2))*(h_copper/1000)/(3*rho_cu*(1+alpha_Cu*dT)) ;  
 eddy_coil= ag_loss+leakage_loss;  
@@ -231,126 +386,7 @@ end
 %----------------------------------------------------------------------------------------------------
 
 
-%--------------------------------------------------------------------------------------------------------------------------
-%% Flux Density Parameters calculation (Airgap, Spacer and steel flux densities)
 
-%% ----------Definition of the parameters/variables----------
-
-% B_ag: flux density in air-gap , B_ag_l: flux density in air-gap included leakage flux
-% B_ag_nl: flux density in air-gap not included leakage flux
-% leakage_insert : adjustment for enable leakage flux effect i.e leakage_insert=1---> leakage flux enabled in calculation otherwise not enabled
-% NI: mmf
-% Br: magnet remanent flux density (constant)
-% phi_ag_nl: airgap flux not included leakage flux
-% phi_ag_l: airgap flux included leakage(first row of flux_l matrix) , magnet_width : magnet width 
-% R: reluctance matrix
-% inverse_R: inverse of reluctance matrix[2x2] , tot_mmf_matrix: total mmf matrix[2x1] , flux_l: flux matrix included leakage effect 
-% S_st: steel reluctance 
-% S_st_A: steel reluctance part A , S_st_C: steel reluctance part C  
-% l_cl:magnet to steel web clearence , tau_pw: web pole pitch
-% l_ws_web: winding to steel web clearence (user defined variable)
-% r_w: web radius
-% S_sp: spacer reluctance
-% inter_area: intermodule area , mu_st: steel relative permeability (constant)
-% S_PM_o: top PM reluctance , S_ag: airgap reluctance ,S_I: flux leakage included reluctance 
-% SI_1: reluctance matrix part one, SI_2: reluctance matrix part two
-% mu_r: magnet relative permeability (constant)
-% B_sp: flux density in spacer(intermodule) , B_sp_l: flux density in spacer included leakage flux
-% B_sp_nl: flux density in spacer(intermodule) not included leakage flux
-% phi_sp_nl: spacer flux not included leakage
-% phi_sp_l: spacer flux included leakage (second row of flux_l matrix )
-% A_sp_o: intermodule area
-% B_st: flux density in steel(intramodule) , B_st_l: flux density in steel included leakage flux
-% B_st_nl: flux density in steel(intramodule) not included leakage flux
-% phi_st_l : steel flux included leakage , A_st: intramodule(web) area 
-% phi_st_nl: steel flux not included leakage 
-
-%% Calculation part
-
-%----for air-gap region-------%
-
-NI=Br*h_m/(mu_0*mu_r) ; 
-magnet_width= width_ratio*tau_p;
-SI_2= pi/(2*l_magnet*mu_0) ; 
-SI_1=(tau_p-magnet_width)/(mu_0*l_magnet*(0.5*h_w+g+h_m-groove)) ;  
-S_I=SI_1+SI_2 ;  
-S_ag=(h_w+2*g)/(l_magnet*magnet_width*mu_0) ; 
-S_PM_o= h_m/(l_magnet*magnet_width*mu_0*mu_r)+0.5*t_o/(l_magnet*magnet_width*mu_0*mu_r) ; 
-
-R(1,1)=2*S_PM_o*(1+2*S_ag/S_I)+S_ag ;  
-
-inter_area=t_o*l_magnet ;
-S_sp=(tau_p/(inter_area*mu_0*mu_st))+(groove_c/(inter_area*mu_0)) ; 
-
-R(1,2)= S_sp ;  
-
-r_w=r_i-l_cl-lc ; 
-diameter_web=r_w;
-tau_pw= 2*pi*r_w/Np ;  
-l_cl= width_winding+l_ws_web ; 
-S_st_A=(l_magnet+2*l_cl+lc)/(t_o*(tau_p+tau_pw)*mu_0*mu_st) ;  
-S_st_C=(2*(h_m+g)+h_w+t_o)/(lc*tau_pw*mu_0*mu_st) ; 
-S_st=2*S_st_A+S_st_C ; 
-R(2,1) = R(1,1)+(1+2*S_ag/S_I)*S_st ; 
-R(2,2)=-2*S_st ; 
-
-inverse_R = inv(R);  
-flux_l= inverse_R*tot_mmf_matrix ; 
-
-phi_ag_l= flux_l(1,:) ; 
-B_ag_l= phi_ag_l/l_magnet/magnet_width ; 
-
-phi_ag_nl=(S_sp+2*S_st)*NI/(S_st*(2*S_PM_o+S_ag+0.5*S_sp)+0.5*(S_sp*(2*S_PM_o+S_ag))) ; 
-B_ag_nl= phi_ag_nl/magnet_width/l_magnet; 
-
-if (leakage_insert==1)  
-    B_ag=B_ag_l ;  
-else
-    B_ag=B_ag_nl; 
-end
-
-%------end of airgap region flux density calculation----------------%
-
-
-%-----for spacer region---------%
-
-A_sp_o=t_i*l_magnet ; 
-
-phi_sp_nl= S_sp*NI/(S_st*(2*S_PM_o+S_ag+0.5*S_sp)+0.5*(S_sp*(2*S_PM_o+S_ag))) ;
-B_sp_nl= phi_sp_nl/A_sp_o ; 
-
-phi_sp_l=flux_l(2,:) ; % 
-B_sp_l=phi_sp_l/A_sp_o ; 
-
-if (leakage_insert==1)  
-    B_sp =B_sp_l ;  
-else
-    B_sp =B_sp_nl ; 
-end
-
-%------ end of spacer region flux density calculation---------%
-
-
-%-----for steel region---------%
-
-A_st=tau_pw*lc ; 
-
-phi_st_nl=S_st*NI/(S_st*(2*S_PM_o+S_ag+0.5*S_sp)+0.5*(S_sp*(2*S_PM_o+S_ag))) ; 
-B_st_nl=phi_st_nl/A_st ; 
-
-
-phi_st_l=phi_ag_l*(1+2*S_ag/S_I)-2*phi_sp_l ; 
-B_st_l=phi_st_l/A_st ;  
-
-if (leakage_insert==1)  
-    B_st =B_st_l ;  
-else
-    B_st =B_st_nl ; 
-end
-
-%------ end of steel region flux density calculation---------%
-
-%----------------------------------------------------------------------------------------------------
 
 
 %--------------------------------------------------------------------------------------------------------------------------
@@ -472,31 +508,7 @@ cost_structure=mass_structure*uc_steel+mass_epoxy*uc_epoxy ;
 
 % bypass thermal model 
 
-% Natural air Cooling for J=5.5 A/mm^2
-% Forced air cooling for J=7 A/mm^2
-% Forced water cooling for J=9 A/mm^2
 
-% thermal constants(from optimization)
-
-alpha_Cu=3.9-E03 ;
-rho_cu=1.7-E08;
-a_bar=40; %heat transfer coefficient(?) in W/m^2.K
-a_stat=25; %heat transfer coefficient(?) in W/m^2.K
-a_conc=60; %heat transfer coefficient(?) in W/m^2.K
-lambda_steel=54; %lambda_steel: thermal conductivity of steel in W/m.K
-lambda_alum=250; % lambda_alum: thermal conductivity of aluminium in W/m.K
-lambda_cu_coil=400; % lambda_cu_coil: thermal conductivity of copper along coil in W/m.K
-lambda_cu_ver=1.8; % lambda_cu_ver: thermal conductivity of copper in vertical in W/m.K
-lambda_epoxy=1.3; % lambda_epoxy:thermal conductivity of epoxy in W/m.K 
-lambda_pm=9; % lambda_pm: lambda_pm:thermal conductivity of PM in W/m.K
-k=0.262; % k: thermal conductiviy of air in W/m.K
-alpha_ep2air=40 ; % alpha_ep2air: epoxy to air heat transfer coefficient in W/m^2.K
-alpha_st2air=40 ; % alpha_st2air: steel to air heat transfer coefficient in W/m^2.K
-alpha_alum2air=40 ;% alpha_alum2air: aluminium to air heat transfer coefficient in W/m^2.K
-h_epoxy=0.0005 ; % h_epoxy: height of the epoxy layer
-h_band=0.01 ; % h_band: height of steel band(jubilee clip)
-w_band=0.04; % w_band: axial width of the steel band
-t_disc=?? ... continue 
 
 % airgap inputs(from optimization)~at 20o C
 
