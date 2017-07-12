98dee5906028984438d53e77892302741e03dd4c
Aydın Başkaya <aydin.baskaya@metu.edu.tr>
2017-07-12 18:18:03 +0300

    code update-1

--- thesis/matlab-optimization/cost.m
+++ thesis/matlab-optimization/cost.m
@@ -16,7 +16,7 @@ penalty_power=0;            %Power per machine penalty
 
 r_mean=x(1);        % mean radius 
 g=x(2);             % air-gap clearence 
-J=x(3);             % curent density in A/mm^2
+J_opt=x(3);             % curent density in A/mm^2
 t_o=x(4);           % outer limb thickness
 t_i=x(5);           % inner limb thickness
 lc=x(6);            % steel web thickness
@@ -35,33 +35,22 @@ n_stack=x(16); % number of parallel machines stacked axially
 
 P_des=5000000;      % Desired rated output power
 rpm=12 ;            % rotational speed
-gear_ratio=1;       % Direct drive
+gear_ratio=1;       % Gearbox ratio of direct drive
+eff_gear=1;         % Efficiency of gearbox
 groove=0 ;          % space between c-cores 
 groove_c=0;         % gap between modules 
 m=3;                % number of phases
-B_opt=1.3;          % Desired remanent flux density for the magnet--Grade N42 rare earth magnet remanent flux density
-t_amb=20;           % ambient temperature
-J_type=7;           %7 A/mm^2 @100 oC is assumed for Forced air cooling 
+t_amb=20;           % ambient temperature in oC
+J_final_type=7;           % 7 A/mm^2 @100 oC is assumed for Forced air cooling 
 alpha_Cu=3.9E-03 ;  % temperature coefficent at 20 Celcius degree
 rho_cu=1.7E-08;     % copper resistivity coefficient
-lambda_steel=54;    % lambda_steel: thermal conductivity of steel in W/m.K
-lambda_alum=250;    % lambda_alum: thermal conductivity of aluminium in W/m.K
-lambda_cu_coil=400; % lambda_cu_coil: thermal conductivity of copper along coil in W/m.K
-lambda_cu_ver=1.8;  % lambda_cu_ver: thermal conductivity of copper in vertical in W/m.K
-lambda_epoxy=1.3;   % lambda_epoxy:thermal conductivity of epoxy in W/m.K 
-lambda_pm=9;        % lambda_pm:thermal conductivity of PM in W/m.K
-alpha_ep2air=40 ;   % alpha_ep2air: epoxy to air heat transfer coefficient in W/m^2.K
-alpha_st2air=40 ;   % alpha_st2air: steel to air heat transfer coefficient in W/m^2.K
-alpha_alum2air=40 ; % alpha_alum2air: aluminium to air heat transfer coefficient in W/m^2.K
-h_epoxy=0.0005 ;    % h_epoxy: height of the epoxy layer
-h_band=0.01 ;       % h_band: height of steel band(jubilee clip)
-w_band=0.04;        % w_band: axial width of the steel band
-t_disc=0.008;       % t_disc: thickness of the disc model, later used in thermal resistance calculations    
+h_band=0.01 ;       % h_band: height of steel band(J_finalubilee clip)
+w_band=0.04;        % w_band: axial width of the steel band(J_finalubilee clip)
 mu_0=1.257E-06 ;    % permeability of air
-Br=1.3 ;            % magnet remanent flux density
+Br=1.3 ;            % magnet remanent flux density--Grade N42 rare earth magnet remanent flux density
 mu_r=1.05 ;         % magnet relative permeability
-mu_st=750 ;         % relative permeability of steel
-l_ws_web=0.02 ;     % winding to steel web clearence 
+mu_st=750 ;         % relative permeability of electrical steel
+coil2web_cl=0.015 ;     % winding to steel web clearence 
 k_leak=0.965 ;      % leakage factor 
 phi=0;              % assume unity power factor
 strand=1 ;          % number of parallel strands in coil (taken as 1) 
@@ -69,24 +58,46 @@ t_epoxy=1 ;         % epoxy thickness on winding surface (in mm)
 d_steel=7850 ;      % volumetric mass density of steel in kg/m^3
 d_copper=8230 ;     % volumetric mass density of copper in kg/m^3
 d_magnet=8400 ;     % volumetric mass density of permanent magnet in kg/m^3
-no_rotor_bar=8 ;    % number of rotor bars, taken as 8 for both end torque arm structure
-no_stator_bar=6 ;   % number of bar in torque arm of stator(opt)
+d_epoxy=900;        % volumetric mass density of epoxy resin in kg/m^3
+no_rotor_bar=8 ;    % number of rotor bars, taken as 8 
+no_stator_bar=6 ;   % number of bar in torque arm of stator(opt) in both sides
 shaft_ro=0.3 ;      % shaft outer radius from 
 shaft_ri=0.1 ;      % shaft inner radius
-d_epoxy=900;        % volumetric mass density of epoxy resin in kg/m^3
 uc_steel=3 ;        % unit cost of steel in £/kg
 uc_copper=8 ;       % unit cost of copper in £/kg
 uc_magnet=110 ;     % unit cost of magnet in £/kg
 uc_epoxy=10 ;       % unit cost of epoxy in £/kg
-%-------------------------------------------------------------------------------------------------------
+%----------------------------End of initialization------------------------
 
+
+%% Variable Control part
+
+Np=round(Np/4)*4;   % fix number of poles at multiple of 4
+fix=(Np*3)/(4*m);
+while ne((fix/n_branch),round(fix/n_branch))        
+    n_branch=n_branch-1;
+end
+
+if (n_branch>fix)||(n_branch<1)
+    n_branch=1;
+end
+
+if pitch_ratio>(0.5-l_magnet/(4*r_mean))        %coil pitch ratio control
+    pitch_ratio=(0.5-l_magnet/(4*r_mean))-0.01;
+end
+%------------End of variable control-------------------------
+
+
+rpm=rpm*gear_ratio;
+P_demand=(P_des*eff_gear)/n_stack;
+J_final=J_opt;
 
 %% [I_ph_rms] Current per phase(rms) calculation
 
 %% ----------Definition of the parameters/variables----------
 
 %I_coil : current in one coil
-% J:curent density, a_cond=cross section area of the conductor 
+% J_final:curent density, a_cond=cross section area of the conductor 
 % a_window: effective area of winding window
 %h_w : height of the winding,  kf: fill factor(1 for concentrated windings)
 %tau_p : pole pitch
@@ -100,7 +111,7 @@ tau_c=tau_p*(4/3);
 width_winding=pitch_ratio*tau_c;
 a_window=h_w*width_winding*kf; 
 a_cond=a_window/Nt;  
-I_coil=J*a_cond*1000000; 
+I_coil=J_final*a_cond*1000000; 
 
 I_ph_rms= I_coil*n_branch;  %%Resulting equation
 
@@ -112,33 +123,27 @@ I_ph_rms= I_coil*n_branch;  %%Resulting equation
 
 %% ----------Definition of the parameters/variables----------
 
-% B_ag: flux density in air-gap , B_ag_l: flux density in air-gap included leakage flux
+% B_ag: flux density in air-gap 
 % B_ag_nl: flux density in air-gap not included leakage flux
-% leakage_insert : adjustment for enable leakage flux effect i.e leakage_insert=1---> leakage flux enabled in calculation otherwise not enabled
 % NI: mmf
-% Br: magnet remanent flux density (constant)
-% phi_ag_nl: airgap flux not included leakage flux
-% phi_ag_l: airgap flux included leakage(first row of flux_l matrix) , magnet_width : magnet width 
-% R: reluctance matrix
-% inverse_R: inverse of reluctance matrix[2x2] , tot_mmf_matrix: total mmf matrix[2x1] , flux_l: flux matrix included leakage effect 
+% Br: magnet remanent flux density of selected magnet (constant)
+% phi_ag_nl: airgap flux not included leakage flux 
 % S_st: steel reluctance 
 % S_st_A: steel reluctance part A , S_st_C: steel reluctance part C  
 % l_cl:magnet to steel web clearence , tau_pw: web pole pitch
-% l_ws_web: winding to steel web clearence (user defined variable)
+% coil2web_cl: winding to steel web clearence (user defined variable)
 % r_w: web radius
 % S_sp: spacer reluctance
 % inter_area: intermodule area , mu_st: steel relative permeability (constant)
-% S_PM_o: top PM reluctance , S_ag: airgap reluctance ,S_I: flux leakage included reluctance 
-% SI_1: reluctance matrix part one, SI_2: reluctance matrix part two
+% S_PM_o: top PM reluctance , S_ag: airgap reluctance 
 % mu_r: magnet relative permeability (constant)
-% B_sp: flux density in spacer(intermodule) , B_sp_l: flux density in spacer included leakage flux
+% B_sp: flux density in spacer(intermodule) 
 % B_sp_nl: flux density in spacer(intermodule) not included leakage flux
 % phi_sp_nl: spacer flux not included leakage
-% phi_sp_l: spacer flux included leakage (second row of flux_l matrix )
 % A_sp_o: intermodule area
-% B_st: flux density in steel(intramodule) , B_st_l: flux density in steel included leakage flux
+% B_st: flux density in steel(intramodule) 
 % B_st_nl: flux density in steel(intramodule) not included leakage flux
-% phi_st_l : steel flux included leakage , A_st: intramodule(web) area 
+% A_st: intramodule(web) area 
 % phi_st_nl: steel flux not included leakage 
 
 %% Calculation part
@@ -150,53 +155,26 @@ r_o= r_mean+ l_magnet/2 ;
 NI=Br*h_m/(mu_0*mu_r) ; 
 magnet_width= width_ratio*tau_p;
 
-% SI_2= pi/(2*l_magnet*mu_0) ; 
-% SI_1=(tau_p-magnet_width)/(mu_0*l_magnet*(0.5*h_w+g+h_m-groove)) ;  
-% S_I=SI_1+SI_2 ;  
-
 S_ag=(h_w+2*g)/(l_magnet*magnet_width*mu_0) ; 
 S_PM_o= h_m/(l_magnet*magnet_width*mu_0*mu_r)+0.5*t_o/(l_magnet*magnet_width*mu_0*mu_r) ; 
-
-% R(1,1)=2*S_PM_o*(1+2*S_ag/S_I)+S_ag ;  
-
+  
 inter_area=t_o*l_magnet ;
 S_sp=(tau_p/(inter_area*mu_0*mu_st))+(groove_c/(inter_area*mu_0)) ; 
 
-%R(1,2)= S_sp ;  
-
-l_cl= width_winding+l_ws_web ; 
+l_cl= width_winding+coil2web_cl ; 
 r_w=r_i-l_cl-lc ; 
-
-%diameter_web=r_w;
-
 tau_pw= 2*pi*r_w/Np ;  
 S_st_A=(l_magnet+2*l_cl+lc)/(t_o*(tau_p+tau_pw)*mu_0*mu_st) ;  
 S_st_C=(2*(h_m+g)+h_w+t_o)/(lc*tau_pw*mu_0*mu_st) ; 
 S_st=2*S_st_A+S_st_C ;
 
-% R(2,1) = R(1,1)+(1+2*S_ag/S_I)*S_st ; 
-% R(2,2)=-2*S_st ; 
-% 
-% inverse_R = inv(R);  
-
-NI=(Br*h_m)/(mu_0*mu_r);
-
-% tot_mmf_matrix=[2*NI;2*NI];
-% flux_l= inverse_R*tot_mmf_matrix ; 
-% phi_ag_l= flux_l(1,:) ; 
-% B_ag_l= phi_ag_l/l_magnet/magnet_width ; 
 
 phi_ag_nl=(S_sp+2*S_st)*NI/(S_st*(2*S_PM_o+S_ag+0.5*S_sp)+0.5*(S_sp*(2*S_PM_o+S_ag))) ; 
 B_ag_nl= phi_ag_nl/magnet_width/l_magnet; 
 B_ag=B_ag_nl;                   %flux density is calculated without leakage
 
-% if (leakage_insert==1)  
-%     B_ag=B_ag_l ;  
-% else
-%     B_ag=B_ag_nl; 
-% end
-
 %------end of airgap region flux density calculation----------------%
+
 
 %-----for spacer region---------%
 
@@ -206,16 +184,8 @@ phi_sp_nl= S_st*NI/(S_st*(2*S_PM_o+S_ag+0.5*S_sp)+0.5*(S_sp*(2*S_PM_o+S_ag))) ;
 B_sp_nl= phi_sp_nl/A_sp_o ; 
 B_sp =B_sp_nl ;                 %flux density is calculated without leakage
 
-% phi_sp_l=flux_l(2,:) ; % 
-% B_sp_l=phi_sp_l/A_sp_o ; 
-
-% if (leakage_insert==1)  
-%     B_sp =B_sp_l ;  
-% else
-%     B_sp =B_sp_nl ; 
-% end
-
 %------ end of spacer region flux density calculation---------%
+
 
 %-----for steel region---------%
 
@@ -224,15 +194,6 @@ A_st=tau_pw*lc ;
 phi_st_nl=S_sp*NI/(S_st*(2*S_PM_o+S_ag+0.5*S_sp)+0.5*(S_sp*(2*S_PM_o+S_ag))) ; 
 B_st_nl=phi_st_nl/A_st ; 
 B_st =B_st_nl ;                 %flux density is calculated without leakage
-
-% phi_st_l=phi_ag_l*(1+2*S_ag/S_I)-2*phi_sp_l ; 
-% B_st_l=phi_st_l/A_st ;  
-
-% if (leakage_insert==1)  
-%     B_st =B_st_l ;  
-% else
-%     B_st =B_st_nl ; 
-% end
 
 %------ end of steel region flux density calculation---------%
 %----------------------------------------------------------------------------------------------------
@@ -243,7 +204,7 @@ B_st =B_st_nl ;                 %flux density is calculated without leakage
 
 %% ----------Definition of the parameters/variables----------
 
-% E_ph_rms: induced emf per phase(rms), lamda: load angle, phi: power factor angle, I_ph_rms: phase current(rms), R_ph_th: phase resistance(including thermal effects), X_ph: phase reactance value
+% E_ph_rms: induced emf per phase(rms), lambda: load angle, phi: power factor angle, I_ph_rms: phase current(rms), R_ph_th: phase resistance(including thermal effects), X_ph: phase reactance value
 % E_ph_peak: induced emf per phase(peak)
 % e:induced emf of one turn,  Nt:number of turns in a coil, N_series: number of coils connnected in series
 % v: air-gap linear speed, flux_lnk_peak : peak flux linkage 
@@ -269,7 +230,7 @@ B_st =B_st_nl ;                 %flux density is calculated without leakage
 
 %winding temperature calculated here
 
-bypass_1=(100-t_amb)/(J_type^2)*(J^2)+t_amb ;      %forced air cooling  last temperature 
+bypass_1=(100-t_amb)/(J_final_type^2)*(J_final^2)+t_amb ;      %forced air cooling  last temperature 
 t_winding=bypass_1 ;
 
 if t_winding>179   
@@ -322,7 +283,6 @@ else
     lambda=asind((I_ph_rms*(R_ph_th*sind(phi)+X_ph*cosd(phi))/E_ph_rms));
 end
     
-    
 V_ph_rms = E_ph_rms*cosd(lambda)-I_ph_rms*(R_ph_th*cosd(phi)+X_ph*sind(phi)) ;  %%Resulting equation
 
 %--------------------------------------------------------------------------------------------------------------------------
@@ -339,10 +299,9 @@ V_ph_rms = E_ph_rms*cosd(lambda)-I_ph_rms*(R_ph_th*cosd(phi)+X_ph*sind(phi)) ;  
 
 %% Calculation part
 
-P_o= m*V_ph_rms*I_ph_rms; %%Resulting equation
+P_o= m*V_ph_rms*I_ph_rms;           %Output power
 
 %---------------------------------------------------------------------------------------------------------------------------
-
 
 
 %--------------------------------------------------------------------------------------------------------------------------
@@ -357,13 +316,9 @@ P_o= m*V_ph_rms*I_ph_rms; %%Resulting equation
 
 %% Calculation part
 
-L_phase=L_coil*N_series/n_branch*1000 ; 
-
+L_phase=L_coil*N_series/n_branch*1000 ;             %in mH
 
 %----------------------------------------------------------------------------------------------------
-
-
-
 
 
 %--------------------------------------------------------------------------------------------------------------------------
@@ -400,7 +355,7 @@ end
 
 h_copper=h_coil_i-2*t_insulation ;
 t_copper=t_coil_i-2*t_insulation;
-ag_loss=2*l_magnet*Nt*(((t_copper/2000)^3)*(B_ag_l^2)*(w_e^2))*(h_copper/1000)/(3*rho_cu*(1+alpha_Cu*dT)) ;  
+ag_loss=2*l_magnet*Nt*(((t_copper/2000)^3)*(B_ag^2)*(w_e^2))*(h_copper/1000)/(3*rho_cu*(1+alpha_Cu*dT)) ;  
 eddy_coil= ag_loss+leakage_loss;  
 P_eddy=eddy_coil*Nc+eddy_magnet; 
 P_copper_th=m*(I_ph_rms*I_ph_rms)*R_ph_th ; 
@@ -409,15 +364,10 @@ P_loss=P_copper_th+P_eddy ;
 Eff=P_o/(P_o+P_loss) ; %%Resulting equation
 
 if (Eff<0.9)
-   penalty_eff=penalty_eff+(abs(0.9-Eff)*10000) ;
+   penalty_eff=((abs(0.9-Eff)^2)*10000) ;
 end
 
 %---------------------------------------------------------------------------------------------------------------------------
-
-
-
-
-
 
 
 %--------------------------------------------------------------------------------------------------------------------------
@@ -428,7 +378,7 @@ end
 % total_mass: total mass of the machine, mass_epoxy: epoxy resin mass
 % m_epoxy_single: epoxy mass in single coil unit
 % tau_former:pitch of the coil former mean value, d_epoxy: volumetric mass density of epoxy resin(taken as constant)
-%mass_structure: total structural mass, m_shaft: shaft mass, m_stator: stator structure mass, m_rotor: rotor torque structure mass, m_steelband: steel band mass   
+% mass_structure: total structural mass, m_shaft: shaft mass, m_stator: stator structure mass, m_rotor: rotor torque structure mass, m_steelband: steel band mass   
 % h_band:height of the band steel structure(opt),w_band: width of the band steel structure(opt) 
 % shaft_ro: shaft outer radius(opt), shaft_ri: shaft inner radius(opt), l_shaft: axial length of the shaft
 % length_total: total axial length of the machine
@@ -451,7 +401,6 @@ end
 % no_rotor_bar: number of rotor bars, taken as 8 for both end torque arm structure
 
 
-
 %% Active mass calculation part
 
 %In this part; steel mass, copper mass and magnet mass are calculated
@@ -461,7 +410,7 @@ h_web=l_mm+2*(h_m-groove);
 m_web_layer=pi*d_steel*t_i*((r_w+lc)^2-r_w^2)*h_web ; 
 number_inner_limb=n_stack-1 ; 
 m_innerlimb_layer=pi*d_steel*t_i*(r_o^2-r_w^2) ;
-number_outer_limb=2 ;
+number_outer_limb=2 ;               %
 m_outerlimb_layer=pi*d_steel*t_o*(r_o^2-r_w^2) ; 
 mass_steel = m_outerlimb_layer*number_outer_limb+m_innerlimb_layer*number_inner_limb+m_web_layer*number_web ; 
 
@@ -474,7 +423,7 @@ m_magnet_layer=pi*d_magnet*h_m*(r_o^2-r_i^2)*l_magnet ;
 mass_magnet=m_magnet_layer*number_magnet_layer ; 
 
 %% Structural mass calculation part
-length_total=2*t_o+n_stack*l_ss+(n_stack-1)*t_i ; 
+length_total=1.25*(2*t_o+n_stack*l_ss+(n_stack-1)*t_i) ; 
 
 length_rotor_bar=r_w ;  
 rotor_alternative_a=length_rotor_bar*0.032 ; 
@@ -486,7 +435,7 @@ rotor_rect_di=rotor_rect_d-2*rotor_alternative_t ;
 m_rotor_torque=no_rotor_bar*(rotor_rect_b*rotor_rect_d-rotor_rect_di*rotor_rect_bi)*length_rotor_bar*d_steel ; 
 m_rotor=2*m_rotor_torque;   
 
-stator_outer=2*(r_o+2*r_w) ; 
+stator_outer=2*(r_o+2*width_winding) ;         %stator outer diameter
 length_stator_bar=0.5*stator_outer ;  
 stator_alternative_a=length_stator_bar*0.025 ; 
 stator_alternative_t=stator_alternative_a*0.4 ;
@@ -496,21 +445,18 @@ stator_rect_d=3*stator_alternative_a ;
 stator_rect_di=stator_rect_d-2*stator_alternative_t ; 
 m_stator_torque=no_stator_bar*(stator_rect_b*stator_rect_d-stator_rect_di*stator_rect_bi)*length_stator_bar*d_steel ; 
 m_stator_cyl=pi*d_steel*length_total*((r_o+width_winding)^2-r_o^2);
-
 m_stator=m_stator_cyl+m_stator_torque*2 ; 
 
-l_shaft=1.25*length_total; 
+l_shaft=length_total; 
 m_shaft=pi*(shaft_ro^2-shaft_ri^2)*l_shaft*d_steel ; 
 
-m_steelband=n_stack*(2*pi*(r_o+0.5*r_w)*h_band*w_band*d_steel) ; 
-
-mass_structure=m_shaft+m_stator+m_rotor+m_steelband ;
+m_steelband=n_stack*(2*pi*(r_o+0.5*width_winding)*h_band*w_band*d_steel) ; 
 
 tau_former=tau_c-2*width_winding ; 
 m_epoxy_single=(l_t*width_winding*(1-kf)+tau_former*l_magnet)*h_w*d_epoxy ; 
-mass_epoxy=m_epoxy_single*Nc; 
+mass_epoxy=m_epoxy_single*Nc;
 
-cost=mass_structure+mass_epoxy+mass_magnet+mass_copper+mass_steel+penalty_eff; %%Resulting total mass equation
+mass_structure=m_shaft+m_stator+m_rotor+m_steelband+mass_epoxy ;
 
 %-------------------------------------------------------------------------------------------------------------------------
 
@@ -552,7 +498,7 @@ d_air=1.205 ; % d_air: density of air in kg/m^3
 dyn_vis=1.82E-5;  % dyn_vis: dynamic viscosity of air in kg/ms
 prandtl_air=0.713 ; % prandtl_air: Prandtl number of air
 lambda_air=0.0257; % thermal conductivity of air in W/m.K
-heatc_air=1005; % heatc_air: heat capacitance of air in J/kgK
+heatc_air=1005; % heatc_air: heat capacitance of air in J_final/kgK
 
 %% Calculation part
 
@@ -606,5 +552,5 @@ beam_y=beam_y1+beam_y2 ;
 beam_y_percent=abs(beam_y)/g ; 
 
 %--------------------------------------------------------------------------------------------------------------------------
-
+cost=mass_structure+mass_magnet+mass_copper+mass_steel+penalty_eff; %%Resulting cost equation
 result=cost; 
