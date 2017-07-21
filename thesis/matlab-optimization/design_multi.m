cb2ec42fc1ebc12d912ef484b5654c25edaf2de9
Aydın Başkaya <aydin.baskaya@metu.edu.tr>
2017-07-21 21:31:23 +0300

    code update for multispeed

--- thesis/matlab-optimization/design_multi.m
+++ thesis/matlab-optimization/design_multi.m
@@ -3,11 +3,19 @@ function [cost_f1,J_final_f,J_init_f,J_pmax_f,P_demand_f,P_net_f,n_stack_f]=desi
 gear_ratio=1;       % Gearbox ratio of direct drive
 eff_gear=1;
 multiwind=1;
-speed_data=[1 6 600 0.0900000000000000 4.54103200838914e-05;1 6 600 0.0900000000000000 4.54103200838914e-05;2.90000000000000 55 16500 0.150000000000000 0.00208130633717835;4.80000000000000 152 76200 0.120000000000000 0.00768948086753894;6.70000000000000 299 209000 0.0900000000000000 0.0158179281625555;8.60000000000000 494 444000 0.0900000000000000 0.0336036368620796;10.5000000000000 738 811000 0.0900000000000000 0.0613796159800598;12 846 1063000 0.0900000000000000 0.0804519504152942;12 916 5000000 0.190000000000000 0.798885260735126];
+speed_data=[1 6 600 0.0900000000000000 4.54103200838914e-05;...             %%power-speed data is taken here
+            1 6 600 0.0900000000000000 4.54103200838914e-05;...
+            2.90000000000000 55 16500 0.150000000000000 0.00208130633717835;...
+            4.80000000000000 152 76200 0.120000000000000 0.00768948086753894;...
+            6.70000000000000 299 209000 0.0900000000000000 0.0158179281625555;...
+            8.60000000000000 494 444000 0.0900000000000000 0.0336036368620796;...
+            10.5000000000000 738 811000 0.0900000000000000 0.0613796159800598;...
+            12 846 1063000 0.0900000000000000 0.0804519504152942;...
+            12 916 5000000 0.190000000000000 0.798885260735126];                
 
 %speed_data(:,1)-->rpm values
 %speed_data(:,2)-->Average torque in kNm
-%speed_data(:,3)-->Average power
+%speed_data(:,3)-->Average power in W
 %speed_data(:,4)-->Time Probability
 %speed_data(:,5)-->Energy ratio
 
@@ -21,14 +29,7 @@ speed_data=[1 6 600 0.0900000000000000 4.54103200838914e-05;1 6 600 0.0900000000
 %speed_data(8,:)-->11 m/s
 %speed_data(9,:)-->12 m/s   --->12 rpm RATED speed
 
-price=10;
-if multiwind==1
-    cost_f2=0;
-    Jmax=7;
-end
 
-for i=1:9           % for all speed values 
-    
 %% Definitions in optimization part(user defined variables/constraints)
 
 %Constraints of optimization (variables):
@@ -51,6 +52,17 @@ h_m=x(14);          % height of the magnet
 l_magnet=x(15);     % axial lenght of the magnet
 n_stack=x(16);      % number of parallel machines stacked axially
 
+
+% price=10;
+if multiwind==1
+    cost_f2=0;
+    Jmax=7;
+end
+
+for i=1:9           % for all speed values 
+    
+
+
 rpm=speed_data(i,1)*gear_ratio;     %take rpm data             
 x(3)=Jmax/2;
 P_demand=speed_data(i,3)*eff_gear/n_stack;  %take P demand
@@ -71,8 +83,8 @@ else
   x(3)=Jmax;
 end
 
-[J_init,J_pmax,P_o,P_loss,cost_f3]=calculate(x,rpm,P_demand);
-
+[J_init,J_pmax,P_o,P_loss,cost_f3]=calculate(x,rpm,P_demand);   %fonk donen degerler degýscek
+% buraya yazdýrma yapýlcak
 income=0;
 
 cost_f2=cost_f2+cost_f3*speed_data(i,5)-income*speed_data(i,4);
