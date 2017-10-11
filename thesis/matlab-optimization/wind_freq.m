219a45ab2c66554f0decc96edc166066f11db8ed
Aydın Başkaya <aydin.baskaya@metu.edu.tr>
2017-10-11 21:16:31 +0300

    wind measurement code update
    excel file for dist. plot

--- thesis/matlab-optimization/wind_freq.m
+++ thesis/matlab-optimization/wind_freq.m
@@ -1,3 +1,6 @@
+speed_0=0;
+speed_1=0;
+speed_2=0;
 speed_3=0;
 speed_4=0;
 speed_5=0;
@@ -9,19 +12,27 @@ speed_10=0;
 speed_11=0;
 speed_12=0;
 
-fileID ='Sample_RES_1.csv';
+fileID ='KORU_RES.csv';
 A=csvread(fileID,1,1);
-plot(A(:,2))
+plot(A(:,1))
 array=A(:,2);
 n=size(array,1);
 
 for i=1:1:n
     
-    if array(i)<=3
-        speed_3=speed_3+1 ;
-    end
+     if array(i)<1
+        speed_0=speed_0+1 ;
+     end
 
-    if (array(i)>3)&(array(i)<4)
+     if (array(i)>=1)&(array(i)<2)
+        speed_1=speed_1+1 ;
+     end
+
+      if (array(i)>=2)&(array(i)<3)
+        speed_2=speed_2+1 ;
+      end
+     
+    if (array(i)>=3)&(array(i)<4)
         speed_3=speed_3+1 ;
     end
 
@@ -63,6 +74,9 @@ for i=1:1:n
 
 end
 
+freq_0=(speed_0/n)*100;
+freq_1=(speed_1/n)*100;
+freq_2=(speed_2/n)*100;
 freq_3=(speed_3/n)*100;
 freq_4=(speed_4/n)*100;
 freq_5=(speed_5/n)*100;
