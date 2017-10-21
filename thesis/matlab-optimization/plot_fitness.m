15e5a493847e3362a7f9a3cb503eb1de51d984ea
Aydın Başkaya <aydin.baskaya@metu.edu.tr>
2017-10-21 16:26:38 +0300

    code update
    Fitness plot function

--- thesis/matlab-optimization/plot_fitness.m
+++ thesis/matlab-optimization/plot_fitness.m
@@ -1,3 +1,18 @@
 A=textread('fitness_list.txt','%.3f');
-linspace=1:1:20101 ;
-scatter(linspace,A,20)
+% linspace=1:1:914453 ;
+% scatter(linspace,A,50)
+
+group=zeros(200,100);
+
+
+for i=1:1:200             %generation
+    for j=1:1:100         %pop. size
+        group(i,j)=A((i*100+1)+j);
+    end
+
+end
+
+group=sort(group,2,'descend');
+
+linspace=1:1:200 ;
+scatter(linspace,group(:,100),15,'filled')
